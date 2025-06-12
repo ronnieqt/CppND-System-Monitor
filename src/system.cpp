dc969abe6962d1e9ccd6e092ab4ac92aeedda005
ronnieqt <xi.ronnie.chen@outlook.com>
2025-06-12 23:14:51 +0100

    cleared TODOs

--- src/system.cpp
+++ src/system.cpp
@@ -17,10 +17,10 @@ using std::vector;
 
 You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
 
-// TODO: Return the system's CPU
+// DONE: Return the system's CPU
 Processor& System::Cpu() { return m_cpu; }
 
-// TODO: Return a container composed of the system's processes
+// DONE: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return m_processes; }
 
 // DONE: Return the system's kernel identifier (string)
@@ -32,11 +32,11 @@ float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 // DONE: Return the operating system name
 std::string System::OperatingSystem() { return m_os; }
 
-// TODO: Return the number of processes actively running on the system
+// DONE: Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // DONE: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// DONE: Return the number of seconds since the system started running
+long int System::UpTime() { return LinuxParser::UpTime(); }
